4097af5459a44ee162679815e5120aeceacde7d5
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-09-25 23:42:55 +0530

    updating the running cript

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from agentic-browser!")
+from mcp_server import server
+import app.run
 
 
 if __name__ == "__main__":
-    main()
+    if (
+        choice := input("Run as (1) API server or (2) MCP server? Enter 1 or 2: ")
+        == "1"
+    ):
+        app.run.run()
+    else:
+        server.run()
