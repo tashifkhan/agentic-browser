906891b0a35ad5c1989f33f8f01fbecc19747f5f
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-11-19 00:51:27 +0530

    updated routers module to include exports with aliases for convenient imports

--- routers/__init__.py
+++ routers/__init__.py
@@ -1,3 +1,15 @@
 """
-adding routers here
+routers package exports with aliases for convenient imports.
 """
+
+from .github import router as github_router
+from .health import router as health_router
+from .website import router as website_router
+from .youtube import router as youtube_router
+
+__all__ = [
+    "github_router",
+    "health_router",
+    "website_router",
+    "youtube_router",
+]
